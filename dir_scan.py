391e38f4bd29896a769b7d45b1686cb137ecce1c
ca1n <eve1ca1n@gmail.com>
2015-06-14 15:24:25 +0800

    Use gevent instead of threading

--- dir_scan.py
+++ dir_scan.py
@@ -1,35 +1,55 @@
 # !/usr/bin/dev python
 # -*- coding:utf-8 -*-
 
+from gevent import monkey; monkey.patch_all()
 import requests
-import threading
-import Queue
 import codecs
+import optparse
+import gevent
 
-filePoint = codecs.open('cgi.txt', 'r')
-rootURL = 'http://www.blackstxz.com'
+filePoint = 0
 pool = list()
-queue = Queue.Queue()
 
 
-def dirScan():
+def openDict(dict_name):
+    global filePoint
+    suc = True
+    try:
+        filePoint = codecs.open(dict_name, 'r')
+    except Exception as e:
+        print e
+        suc = False
+        filePoint.close()
+    return suc
+
+
+def dirScan(target_url):
     global filePoint
 
     for line in filePoint:
-        target_url = rootURL + line
+        target_url = target_url + line
         req = requests.get(target_url)
         if req.status_code == 200:
-            print 'yes'
-            print target_url,
+            print '[!]sensetive file found at url : %s' % (target_url)
+
 
 if __name__ == "__main__":
-    for i in range(5):
-        pool.append(threading.Thread(target=dirScan))
 
-    for thread in pool:
-        thread.start()
+    # 解析传入的参数
+    parser = optparse.OptionParser('python dir_scan.py -u url --gevent 10 \
+--dict dir.txt')
+    parser.add_option('-u', dest='url', help=\
+            'url to start sensitive file search')
+    parser.add_option('--gevet', dest='gevent_num', help=\
+            'gevent nubmer for scanning', default=10)
+    parser.add_option('--dict', dest='dic', help=\
+            'dictionary used', default='dir.txt')
+    (options, args) = parser.parse_args()
 
-    for thread in pool:
-        thread.join()
+    if options.url and openDict(options.dic):
+        for i in range(options.gevent_num):
+            pool.append(gevent.spawn(dirScan, options.url))
+        gevent.joinall(pool)
+    else:
+        print parser.print_help()
 
-    print 'End...'
